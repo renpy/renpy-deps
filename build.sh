be988bafd1b7d752568eb56fcc3d9621c4a42fb8
tom <>
2009-04-14 11:47:27 -0400

    updating

--- build.sh
+++ build.sh
@@ -81,7 +81,6 @@ export RENPY_DEPS_INSTALL="$INSTALL"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 export DYLIB_LIBRARY_PATH="$DYLIB_LIBRARY_PATH"
 export DYLD_FRAMEWORK_PATH="$DYLD_FRAMEWORK_PATH"
-export FFMPEG_BUILD_PATH="$BUILD/ffmpeg-0.5"
 export PYTHONPATH="$INSTALL/python"
 EOF
 
@@ -100,12 +99,6 @@ if [ \! -e built.sdl ]; then
    try patch -p0 < $SOURCE/sdl-staticgray.diff
    try patch -p0 < $SOURCE/sdl-no-asm-stretch.diff
 
-   if [ "x$NOALTIVEC" != "x" ]; then
-       echo Disabling altivec support...
-       try sed -e "s/-faltivec//g" configure > configure.new
-       try cat configure.new > configure
-   fi
-
    try ./configure --prefix="$INSTALL"  --disable-debug --disable-video-dummy --disable-video-fbcon
 
    try make
@@ -114,33 +107,6 @@ if [ \! -e built.sdl ]; then
    touch built.sdl
 fi
 
-# if [ "x$NOSMPEG" = "x" -a \! -e built.smpeg ]; then
-#    try $CP "$SOURCE/smpeg" "$BUILD"
-#    try cd "$BUILD/smpeg"
-   
-#    # try aclocal
-#    # try autoconf
-#    # try automake --foreign
-   
-#    try sh ./configure --prefix="$INSTALL" --disable-opengl-player --disable-gtk-player --disable-gtktest --enable-mmx
-
-
-#    # libtool
-#    # cp ../SDL-1.2.11/libtool .
-
-
-
-#    if [ $MAC = no ] ; then
-#        try make CXXLD="${CXXLD:-g++} -no-undefined"
-#    else
-#        try make CXXLD="${CXXLD:-g++}"
-#    fi
-
-#    try make install
-#    cd "$BUILD"
-
-#    touch built.smpeg
-# fi
 
 # This will be built shared on Linux and Mac by build_python, and 
 # static on windows here.
@@ -154,21 +120,6 @@ if [ \! -e built.zlib ]; then
    touch built.zlib
 fi
 
-# if [ \! -e built.freetype ]; then
-#    try tar xvjf "$SOURCE/freetype-2.1.10.tar.bz2"
-#    cd "$BUILD/freetype-2.1.10"
-#    for i in bdf cff otvalid pcf pfr psaux pshinter psnames type1 type42 winfonts; do
-#        rm -Rf src/$i
-#    done
-   
-#    cp "$SOURCE/ftmodule.h" include/freetype/config
-      
-#    try ./configure --prefix="$INSTALL"
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.freetype
-# fi
 
 if [ \! -e built.freetype ]; then
    try tar xjf "$SOURCE/freetype-2.3.7.tar.bz2"
@@ -231,100 +182,6 @@ if [ \! -e built.sdl_image ]; then
    touch built.sdl_image
 fi
 
-# if [ \! -e built.ogg ]; then
-#    try tar xvzf "$SOURCE/libogg-1.1.3.tar.gz"
-#    try cd "$BUILD/libogg-1.1.3"
-#    try ./configure --prefix="$INSTALL" --disable-shared
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.ogg
-# fi
-
-# if [ \! -e built.vorbis ]; then
-#    try tar xvzf "$SOURCE/libvorbis-1.2.0.tar.gz"
-#    try cd "$BUILD/libvorbis-1.2.0"
-#    try ./configure --prefix="$INSTALL" --with-ogg-libraries="$INSTALL/lib" --with-ogg-includes="$INSTALL/include" --disable-shared
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.vorbis
-# fi
-
-# if [ \! -e built.speex ]; then
-#    try tar xvzf "$SOURCE/speex-1.0.5.tar.gz"
-#    try cd "$BUILD/speex-1.0.5"
-#    try ./configure --prefix="$INSTALL" --with-ogg-libraries="$INSTALL/lib" --with-ogg-includes="$INSTALL/include" --disable-shared
-#    try patch -p0 < "$SOURCE/speex.patch"
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.speex
-# fi
-
-# if [ \! -e built.sdl_mixer ]; then
-#    try tar xvzf "$SOURCE/SDL_mixer-1.2.6.tar.gz"
-#    try patch -p0 < "$SOURCE/SDL_mixer.patch"
-#    try cd "$BUILD/SDL_mixer-1.2.6"
-#    try ./configure --prefix="$INSTALL" --disable-music-midi
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.sdl_mixer
-# fi
-
-
-# Flac
-
-# if [ \! -e built.flac ]; then
-#    try tar xvzf "$SOURCE/flac-1.1.2.tar.gz"
-#    try cd "$BUILD/flac-1.1.2"
-#    try ./configure --prefix="$INSTALL" --disable-shared --disable-asm-optimizations
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.flac
-# fi
-
-
-# if [ \! -e built.modplug ]; then
-#    try tar xvzf "$SOURCE/libmodplug-0.7.tar.gz"
-#    try cd "$BUILD/libmodplug-0.7"
-#    try ./configure --prefix="$INSTALL" --disable-shared
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.modplug
-# fi
-
-
-
-# if [ \! -e built.sdl_sound ]; then
-#    try $CP -p "$SOURCE/SDL_sound-1.0.1" . 
-#    try cd "$BUILD/SDL_sound-1.0.1"
-
-# # cp "$SOURCE/SDL_sound.configure" ./configure
-
-#    export CPPFLAGS="-DSDLSOUND_MINGW_FIX -I$INSTALL/include -I$INSTALL/include/libmodplug" 
-#    export LDFLAGS="$LDFLAGS -L$INSTALL/lib"
-
-#    MIDI=`python "$SOURCE/midi_flag.py"`
-
-#    try ./configure --prefix="$INSTALL" --disable-mikmod --disable-smpeg --enable-mpglib $MIDI --disable-flac --disable-shared
-
-#    # try make clean SED=sed
-#    try make SED=sed
-#    try make install
-
-#    unset CPPFLAGS
-#    LDFLAGS="$OLD_LDFLAGS"
-
-#    cd "$BUILD"
-#    touch built.sdl_sound
-# fi
-
-
-# unset MACOSX_DEPLOYMENT_TARGET
 
 if [ \! -e built.pygame ]; then
     
@@ -343,20 +200,12 @@ if [ \! -e built.pygame ]; then
    try cp "$SOURCE/pygame_init.py" lib/__init__.py
    # try cp "$SOURCE/config"*.py .
 
+   try grep -v "install_parachute ()" src/base.c > src/base.c.new
+   try mv src/base.c.new src/base.c
 
    try python "$SOURCE/edit.py" "$SOURCE/Setup" Setup @SDL@ "$SDL" @INSTALL@ "$INSTALL"
    
-   #   export SDL_CONFIG="$INSTALL/bin/sdl-config"
-   # export SMPEG_CONFIG="$INSTALL/bin/smpeg-config"
-
-   # try python config.py --auto
-   # try touch config_msys.py
-
-   # try rm mingw32*.py
-
    if [ "x$MSYSTEM" != "x" ]; then
-       # try python setup.py build --compiler=mingw32 install_lib -d "$INSTALL/python"
-       echo $CFLAGS
        try python setup.py build --compiler=mingw32 install_lib -d "$INSTALL/python"
        try cp "$INSTALL/bin/"*.dll "$INSTALL/python/pygame"
        try strip "$INSTALL/python/pygame/"*.dll
@@ -377,27 +226,11 @@ if [ \! -e built.pygame ]; then
    touch built.pygame
 fi
 
-# if [ \! -e built.ffmpeg ]; then
-#    try $CP "$SOURCE/ffmpeg" . 
-#    try cd "$BUILD/ffmpeg"
-
-#    try ./configure --prefix="$INSTALL" \
-#        --disable-encoders --disable-muxers \
-#        --enable-static --disable-shared \
-#        --disable-v4l --disable-v4l2 --disable-bktr \
-#        --disable-dv1394  --disable-network \
-#        --enable-libogg --enable-vorbis --enable-memalign-hack
-
-#    try make
-#    try make install
-#    cd "$BUILD"
-#    touch built.ffmpeg
-# fi
-
-
 if [ \! -e built.ffmpeg ]; then
    try tar xjf "$SOURCE/ffmpeg-0.5.tar.bz2" 
    try cd "$BUILD/ffmpeg-0.5"
+
+   try patch -p0 < "$SOURCE/ffmpeg-ogg-size.patch"
 
    export CFLAGS="$CFLAGS -fno-common"
    export CXXFLAGS="$CXXFLAGS -fno-common"
@@ -462,22 +295,13 @@ if [ \! -e built.ffmpeg ]; then
 
    try make
    try make install
+
+   try mkdir -p "$INSTALL/include/libswscale"
+   try cp libswscale/swscale.h  "$INSTALL/include/libswscale"
+
    cd "$BUILD"
    touch built.ffmpeg
 fi
-
-# if [ "x$MSYSTEM" != "x" ]; then
-#     echo
-#     echo To install pygame globally, run:
-#     echo
-#     echo . env.sh
-#     echo "cd '$BUILD/pygame-1.7.1release'"
-#     echo python setup.py build --compiler=mingw32 install
-#     echo "cd ../.."
-#     echo
-#     echo unset PYTHONPATH to use it. Remember to strip the .pyds before
-#     echo distributing them.
-# fi
 
 echo
 cat ../env.sh
