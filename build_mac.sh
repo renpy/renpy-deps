48819eb8d5ec2cc35e4521f094131158e6717001
Tom Rothamel <tom@rothamel.us>
2014-07-22 23:02:18 -0400

    Changes to build on OS X 10.9 using clang-as-gcc.

--- build_mac.sh
+++ build_mac.sh
@@ -8,13 +8,18 @@ try () {
 
 DIR=`dirname $0`
 
+# Note - I had to link this into the SDKs directory of XCode to make it work.
+# I'm not sure why.
 export SDKROOT="/Users/tom/SDKs/MacOSX10.6.sdk"
 export MACOSX_DEPLOYMENT_TARGET=10.6
-export CFLAGS="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export EXTRA_CFLAGS="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export CXXFLAGS="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+
+export PATH="$SDKROOT/usr/bin:$PATH"
+
+export CFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export EXTRA_CFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export CXXFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 # export LDFLAGS="-Wl,-syslibroot,$SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export LDFLAGS="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export LDFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 
 export JPEG_ASM="--host x86_64-apple-darwin"
 
