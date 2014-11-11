0a265ac6552b5642f4c9526ef67e863d83c1e386
Tom Rothamel <tom@rothamel.us>
2014-11-11 00:26:44 -0500

    Support for building on mac with SDL2.

--- build_mac.sh
+++ build_mac.sh
@@ -10,16 +10,16 @@ DIR=`dirname $0`
 
 # Note - I had to link this into the SDKs directory of XCode to make it work.
 # I'm not sure why.
-export SDKROOT="/Users/tom/SDKs/MacOSX10.6.sdk"
+# export SDKROOT="/Users/tom/SDKs/MacOSX10.6.sdk"
 export MACOSX_DEPLOYMENT_TARGET=10.6
 
 export PATH="$SDKROOT/usr/bin:$PATH"
 
-export CFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export EXTRA_CFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export CXXFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export CFLAGS="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export EXTRA_CFLAGS="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export CXXFLAGS="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 # export LDFLAGS="-Wl,-syslibroot,$SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export LDFLAGS="--sysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+export LDFLAGS="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 
 export JPEG_ASM="--host x86_64-apple-darwin"
 
--- scripts/build_renpy_mac.sh
+++ scripts/build_renpy_mac.sh
@@ -3,6 +3,7 @@
 BASE=/Users/tom
 AB=/Volumes/shared/ab
 RENPY="${1:-/Volumes/shared/ab/renpy}"
+PYGAME_SDL2="${2:-/Volumes/shared/ab/pygame_sdl2}"
 
 
 try () {
@@ -11,14 +12,24 @@ try () {
 
 . "$BASE/newbuild/env.sh"
 
+INCLUDE="$BASE/newbuild/install/include/pygame_sdl2"
+
 export RENPY_CC="ccache gcc"
 export RENPY_LD="ccache gcc"
 
-try cd "$RENPY/module"
-try python setup.py clean --all
-try python setup.py install_lib -d $PYTHONPATH
+set -e
 
-try cd "$AB/renpy-deps/renpython"
-try python -O build.py darwin-x86_64 "$RENPY" renpy.py
+export PYGAME_SDL2_INSTALL_HEADERS=1
+
+cd "$PYGAME_SDL2"
+python setup.py clean --all
+python setup.py install_lib -d $PYTHONPATH install_headers -d $INCLUDE
+
+cd "$RENPY/module"
+python setup.py clean --all
+python setup.py install_lib -d $PYTHONPATH
+
+cd "$AB/renpy-deps/renpython"
+python -O build.py darwin-x86_64 "$RENPY" renpy.py
 
 echo done.
