ffd00549cf28874db53220663179a48de2133564
Tom Rothamel <tom@rothamel.us>
2015-02-14 19:49:35 -0500

    Add Ren'Py icon.

--- windows/main/build.sh
+++ windows/main/build.sh
@@ -3,7 +3,11 @@ set -ex
 CFLAGS="$(sdl2-config --cflags) -Ic:/python27/include"
 LDFLAGS="$(sdl2-config --libs) -Lc:/python27/libs -lpython27"
 
-gcc -o main -std=c99 main.c $CFLAGS $LDFLAGS
+# . /opt/windows_32/bin/win-builds-switch 32
+
+windres -o renpy_icon.o renpy_icon.rc
+gcc -c main.c -std=c99 $CFLAGS
+gcc -o main renpy_icon.o main.o $LDFLAGS
 cp main.exe /t/ab/renpy/lib/windows-i686/renpy.exe
 /t/ab/renpy/lib/windows-i686/renpy.exe
 
