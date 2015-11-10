f00c99fe0a445a3005326021c4093498d3afeaa6
Tom Rothamel <tom@rothamel.us>
2015-11-10 09:50:20 -0500

    Rewrite remote.py to use threads.

--- scripts/remote.py
+++ scripts/remote.py
@@ -11,84 +11,93 @@ import threading
 SECRET="I didn't really want to write this code, but ssh on windows sucked so bad."
 
 
+def process_socket(s):
+
+    f = s.makefile("rb+")
+
+    secret = f.readline()
+    if secret[:-1] != SECRET:
+        print "Bad secret.", repr(secret)
+        f.close()
+        s.close()
+        return
+
+    cmd = [ ]
+
+    for l in f:
+        l = l[:-1]
+        if not l:
+            break
+
+        cmd.append(l)
+
+    print cmd
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    lock = threading.Lock()
+
+    def stdout_thread():
+        while True:
+            l = proc.stdout.readline()
+            if not l:
+                break
+
+            with lock:
+                f.write("O " + l)
+                f.flush()
+                sys.stdout.write(l)
+
+
+    def stderr_thread():
+        while True:
+            l = proc.stderr.readline()
+            if not l:
+                break
+
+            with lock:
+                f.write("E " + l)
+                f.flush()
+                sys.stdout.write(l)
+
+    t1 = threading.Thread(target=stdout_thread)
+    t2 = threading.Thread(target=stderr_thread)
+
+    t1.daemon = True
+    t2.daemon = True
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    proc.wait()
+
+    f.write("R %d\n" % proc.returncode)
+
+    f.close()
+    s.close()
+
+    print
+    print "Done."
+    print
+
 def server():
     ss = socket.socket()
     ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     ss.bind(('0.0.0.0', 22222))
     ss.listen(1)
-    
+
     while True:
         print
         print "Waiting..."
         print
-        
-        try:
 
-            s, addr = ss.accept()
+        s, addr = ss.accept()
 
-            f = s.makefile("rb+")
+        t = threading.Thread(target=process_socket, args=(s,))
+        t.daemon = True
+        t.start()
 
-            secret = f.readline()
-            if secret[:-1] != SECRET:
-                print "Bad secret.", repr(secret)
-                f.close()
-                s.close()
-                continue
-
-            cmd = [ ]
-            
-            for l in f:
-                l = l[:-1]
-                if not l:
-                    break
-
-                cmd.append(l)
-
-            print cmd
-                
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            lock = threading.Lock()
-            
-            def stdout_thread():
-                while True:
-                    l = proc.stdout.readline()
-                    if not l:
-                        break
-                    
-                    with lock:                        
-                        f.write("O " + l)
-                        sys.stdout.write(l)
-
-
-            def stderr_thread():
-                while True:
-                    l = proc.stderr.readline()
-                    if not l:
-                        break
-                    
-                    with lock:                        
-                        f.write("E " + l)
-                        sys.stdout.write(l)
-                        
-            t1 = threading.Thread(target=stdout_thread)
-            t2 = threading.Thread(target=stderr_thread)
-            t1.start()
-            t2.start()
-
-
-            t1.join()
-            t2.join()
-
-            proc.wait()
-
-            f.write("R %d\n" % proc.returncode)
-
-            f.close()
-            s.close()
-        except Exception:
-            traceback.print_exc()
-            continue
-
-        
 server()
