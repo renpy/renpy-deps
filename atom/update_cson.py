2576c6b902c2095895919042326926bb9345795c
Tom Rothamel <pytom@bishoujo.us>
2018-02-13 01:10:16 -0500

    Use a template to generate the cson.

--- atom/update_cson.py
+++ atom/update_cson.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 import os
+import jinja2
+
 
 D = os.path.dirname(__file__)
 
 
 def main():
+    env = jinja2.Environment(
+        autoescape=jinja2.select_autoescape(default=False),
+        block_start_string="<<%",
+        block_end_string="%>>",
+        variable_start_string="<<<",
+        variable_end_string=">>>",
+    )
+
     src = os.path.join(D, "language-renpy", "source", "renpy.tmpl.cson")
     dst = os.path.join(D, "language-renpy", "grammars", "renpy.cson")
 
     with open(src) as f:
-        data = f.read()
+        template = env.from_string(f.read())
+
+    data = template.render()
 
     with open(dst, "w") as f:
         f.write(data)
